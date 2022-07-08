aa4d73e78d87fd09cdf1a5695efda863c7684987
Russell Smith <resmithjr88@gmail.com>
2022-07-07 20:09:07 -0600

    26.2 complete

--- src/App.js
+++ src/App.js
@@ -1,8 +1,16 @@
-import React from "react"
+import React, { useState } from "react"
 
 function App() {
+  const [subscribed, setSubscribed] = useState(false)
+  console.log("Subscribed status:", subscribed)
+
   return (
-    <h1>Hello there</h1>
+    <section>
+      <p>Please click to subscribe to my updates!</p>
+      <button onClick={() => setSubscribed(!subscribed)}>
+        {subscribed ? "Unsubscribe" : "Subscribe"}
+      </button>  
+    </section>
   );
 }
 
